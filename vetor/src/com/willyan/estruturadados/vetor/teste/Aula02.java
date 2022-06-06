67607c748369586e879e04b783c42aa95610dbcf
Willyan Ferdinando <willyantferdinando@gmail.com>
2022-06-06 12:38:32 -0300

    Aula-02

---
+++ vetor/src/com/willyan/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,15 @@
+package com.willyan.estruturadados.vetor.teste;
+
+import com.willyan.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+
+		int i = 0;
+		i++;
+	}
+
+}
