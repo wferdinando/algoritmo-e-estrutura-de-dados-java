5df0be05543b6df230822528d5e07bd24769d9d7
Willyan Ferdinando <willyantferdinando@gmail.com>
2022-06-06 14:02:45 -0300

    Aula-03 Adicionando Elementos no Final do Vetor

--- src/com/willyan/estruturadados/vetor/Vetor.java
+++ src/com/willyan/estruturadados/vetor/Vetor.java
@@ -3,9 +3,37 @@ package com.willyan.estruturadados.vetor;
 public class Vetor {
 
 	private String[] elementos;
-	
+	private int tamanaho;
+
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
+		this.tamanaho = 0;
 	}
-	
+
+	// Adicionar Elemento Final do Vetor - Opção 1
+	/*
+	 * public void adiciona(String elemento) { for(int i=0; i<this.elementos.length;
+	 * i++) { if(this.elementos[i] == null) { this.elementos[i] = elemento; break; }
+	 * } }
+	 */
+
+	/*
+	 * public void adiciona(String elemento) throws Exception {
+	 * 
+	 * if (this.tamanaho < this.elementos.length) { this.elementos[this.tamanaho] =
+	 * elemento; this.tamanaho++; }else { throw new
+	 * Exception("Vetor já esta cheio, não é possível adicionar mais elementos."); }
+	 * }
+	 */
+
+	public boolean adiciona(String elemento) {
+
+		if (this.tamanaho < this.elementos.length) {
+			this.elementos[this.tamanaho] = elemento;
+			this.tamanaho++;
+			return true;
+		}
+		return false;
+	}
+
 }
---
+++ src/com/willyan/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,17 @@
+package com.willyan.estruturadados.vetor.teste;
+
+import com.willyan.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(2);
+
+		vetor.adiciona("Elemento 1");
+		vetor.adiciona("Elemento 2");
+		vetor.adiciona("Elemento 3");
+
+	}
+
+}
